9e2561e6ed7a0e77730f66e758917147c489ca30
ChiyanMani <manivelkumar500@gmail.com>
2016-11-07 12:43:07 +0530

    Add files via upload

---
+++ bal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Addsample.Models;
+
+namespace Addsample.Models
+{
+    public class bal
+    {
+        public string save(Class1 obj)
+        {           
+            dal d = new dal();
+            return d.save(obj);            
+        }
+    }
+}
