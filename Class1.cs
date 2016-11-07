25d6a5a826650ebd27ad7c94d3231a1da4967ce4
ChiyanMani <manivelkumar500@gmail.com>
2016-11-07 12:33:00 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Addsample.Models
+{
+    public class Class1
+    {
+        public string name { get; set; }
+        public Int64 age { get; set; }
+        public Int64 id { get; set; }
+        public string place { get; set; }
+        public string country { get; set; }
+
+        public byte image { get; set; }   
+    }
+}
